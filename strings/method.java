cdb018963ac893c6d55017a1599ff65afb80674f
sachin dixit <sachindixit13122001@gmail.com>
2026-07-29 02:18:17 +0530

    learn method in java

---
+++ strings/method.java
@@ -0,0 +1,17 @@
+public class method{
+    public static void main(String[] args){
+        String name = "sachin dixit";
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.charAt(9));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.lastIndexOf('i'));
+        System.out.println(name.substring(4));
+        System.out.println(name.substring(3, 5));
+        System.out.println(name.indexOf("h"));
+        System.out.println(name.replace("s" , "a"));
+
+
+    }
+}
