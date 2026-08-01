8e7ad42b635672efd00a81bfee2cf84f8abc8265
sachin dixit <sachindixit13122001@gmail.com>
2026-08-01 23:07:29 +0530

    Remove_Duplicate_Characters

---
+++ strings/coding questions/Remove_Duplicate_Characters.java
@@ -0,0 +1,13 @@
+public class Remove_Duplicate_Characters{
+    public static void main(String[] args){
+        String str = "aabbccdde";
+        String result = "";
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(result.indexOf(ch) == -1){
+                result = result + ch;
+            }
+        }
+        System.out.println(result);
+    }
+}
